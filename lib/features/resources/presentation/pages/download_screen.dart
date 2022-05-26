6f91bbfbb02667904a31aa1da5aebab1d4de3d6e
Ademola <kolawoleoluwasegun567@gmail.com>
2022-05-26 07:02:17 +0100

    unrelivant code

--- lib/features/resources/presentation/pages/download_screen.dart
+++ lib/features/resources/presentation/pages/download_screen.dart
@@ -1,9 +1,7 @@
 import 'dart:io';
 import 'dart:math';
-
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:liquid_progress_indicator/liquid_progress_indicator.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
